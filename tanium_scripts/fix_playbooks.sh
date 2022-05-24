7886b33172ee3e2ada7ed0f4f0da2d533acfeb63
Ian Hands <ian.hands@tanium.com>
2022-05-24 15:26:50 -0400

    Tanium: initial finished with fix_playbooks.sh script

--- tanium_scripts/fix_playbooks.sh
+++ tanium_scripts/fix_playbooks.sh
@@ -2,9 +2,13 @@
 
 for var in `find ./Solutions/Tanium/Playbooks -type f -name azuredeploy.json`
 do
-  cat $var | jq >/tmp/tmp.orig
+  NAME=`echo "$var" | sed 's|./Solutions/Tanium/Playbooks/Tanium-||' | cut -d/ -f1`
+  NAME=`echo "${NAME}Tanium"`
+  # cat $var | jq >/tmp/tmp.orig
   cat $var |
     jq '.resources[0] .properties .definition .actions .HTTP .inputs .headers .Authorization = "__CHANGE_ME__"' |
+    jq '.parameters = {}' |
+    jq '.parameters .PlaybookName = { "defaultValue": "'"$NAME"'", "type": "string" }' |
     jq 'del(.resources[0] .tags)' |
     jq $'.variables .AzureSentinelConnectionName = "[concat(\'azuresentinel-\', parameters(\'PlaybookName\'))]"' |
     jq $'.resources[0] .name = "[variables(\'AzureSentinelConnectionName\')]"' |
