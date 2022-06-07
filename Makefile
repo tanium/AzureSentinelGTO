aa0e61a8ef9a338c3f0883ddea5fec1091460c2c
Ian Hands <ian.hands@tanium.com>
2022-06-07 15:57:06 -0400

    Working on createUiDef json
    We need to re-write some of this file to do things like collect the
    API tokens only once.

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 .PHONY: all
-all: Solutions/Tanium/Packages/1.0.0.zip
+all: Solutions/Tanium/Packages/1.0.1.zip
 
-Solutions/Tanium/Packages/1.0.0.zip: Tools/Create-Azure-Sentinel-Solution/input/Solution_Tanium.json
+Solutions/Tanium/Packages/1.0.1.zip: Tools/Create-Azure-Sentinel-Solution/input/Solution_Tanium.json
+	rm -f ./Solutions/Tanium/Package/*zip && \
 	./tanium_scripts/pre-build.sh && \
 	./tanium_scripts/fix_playbooks.sh && \
-	./tanium_scripts/build.sh
+	./tanium_scripts/build.sh && \
+	./tanium_scripts/fix_createuidef.sh
 
 .PHONY: dev-server
 dev-server:
---
+++ tanium_scripts/fix_createuidef.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+GEN_FILE="Solutions/Tanium/Package/createUiDefinition.json"
+ORIG_FILE="Solutions/Tanium/Package/createUiDefinition.original.json"
+NEW_FILE="Solutions/Tanium/Package/createUiDefinition.custom.json"
+ZIP_FILE="Solutions/Tanium/Package/1.0.1.zip"
+
+cat "$GEN_FILE" | jq > "$ORIG_FILE"
+cat "$GEN_FILE" |
+  jq '.parameters .basics += [{
+        "name": "taniumserverhostname",
+        "type": "Microsoft.Common.TextBox",
+        "label": "Tanium Server Hostname",
+        "placeholder": "host.example.com",
+        "constraints": {
+          "required": true
+        },
+        "visible": true
+      },
+      {
+        "name": "taniumforwarderapikey",
+        "type": "Microsoft.Common.TextBox",
+        "label": "Tanium Forwarder API Key",
+        "placeholder": "... Tanium forwarder api key...",
+        "constraints": {
+          "required": true
+        },
+        "visible": true
+      }] ' |
+  sed -r \
+    -e 's/A P I/API/g' \
+    -e 's/__Change_Me__/[basics('\'taniumforwarderapikey\'').value]/' > "$NEW_FILE"
+
+diff -u  "$ORIG_FILE" "$NEW_FILE" || true
+
+cp "$NEW_FILE" ./createUiDefinition.json
+cp "$ZIP_FILE" ./.tmp.zip
+zip .tmp.zip createUiDefinition.json
+cp  .tmp.zip "$ZIP_FILE"
+rm ./createUiDefinition.json .tmp.zip
+rm "$NEW_FILE" "$ORIG_FILE"
